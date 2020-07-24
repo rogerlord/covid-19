bede024c8e54c92364f775d2708bb33355c842f3
rlordcardano <32603156+rogerlord@users.noreply.github.com>
2020-07-24 22:38:39 +0200

    New commit

--- covid_19/dataretrieval.py
+++ covid_19/dataretrieval.py
@@ -4,3 +4,9 @@ import pandas as pd
 def get_latest_rivm_file():
     url = "https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.csv"
     return pd.read_csv(url)
+
+
+def update_files(folder):
+    df_daily_cases = pd.read_csv(folder + r"\COVID-19_daily_cases.csv")
+    df_lagged = pd.read_csv(folder + r"\COVID-19_lagged.csv")
+
