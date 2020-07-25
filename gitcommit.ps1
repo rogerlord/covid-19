d9d2ff4723c672eac6397c9a2aebb07d348a5989
rogerlord <32603156+rogerlord@users.noreply.github.com>
2020-07-25 14:24:42 +0200

    Added git checkout

--- gitcommit.ps1
+++ gitcommit.ps1
@@ -1,3 +1,4 @@
+git checkout master
 git config --global credential.helper store
 Add-Content -Path "$HOME\.git-credentials" -Value "https://$($env:access_token):x-oauth-basic@github.com`n" -NoNewline
 git config --global user.email "32603156+rogerlord@users.noreply.github.com"
