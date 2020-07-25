c3190325a1b9b9bb9a263d0d846aaf1291abf20e
rlordcardano <32603156+rogerlord@users.noreply.github.com>
2020-07-25 12:59:35 +0200

    Add date as commit message

--- gitcommit.ps1
+++ gitcommit.ps1
@@ -4,7 +4,7 @@ git config --global user.email "32603156+rogerlord@users.noreply.github.com"
 git config --global user.name "AppVeyor"
 git add -Af .\data
 if (git status --porcelain) {
-    git commit -m "Updated files"
+    git commit -m %date%
     git fetch --all
     git checkout -f master
     git merge HEAD@{1} -s recursive -X ours
