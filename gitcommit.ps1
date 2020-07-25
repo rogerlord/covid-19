c9d8bd3ff3ee8a03ca65e572d2266f07ed622f57
rlordcardano <32603156+rogerlord@users.noreply.github.com>
2020-07-25 12:49:46 +0200

    Add gitcommit.ps1

---
+++ gitcommit.ps1
@@ -0,0 +1,12 @@
+git config --global credential.helper store
+Add-Content -Path "$HOME\.git-credentials" -Value "https://$($env:access_token):x-oauth-basic@github.com`n" -NoNewline
+git config --global user.email "32603156+rogerlord@users.noreply.github.com"
+git config --global user.name "AppVeyor"
+git add -Af .\data
+if (git status --porcelain) {
+    git commit -m "Updated files"
+    git fetch --all
+    git checkout -f master
+    git merge HEAD@{1} -s recursive -X ours
+    git push --all
+}
