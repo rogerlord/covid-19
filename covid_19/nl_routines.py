5e11789d5d3fa1dc00e6802df70a7d82d35a6dd0
rogerlord <32603156+rogerlord@users.noreply.github.com>
2020-07-25 16:49:06 +0200

    Add plot generation

--- covid_19/nl_routines.py
+++ covid_19/nl_routines.py
@@ -1,7 +1,10 @@
 from dataretrieval import update_files
+from plotting import generate_plot
 import sys
 
 
 if __name__ == "__main__":
     folder = sys.argv[1]
     update_files(folder)
+    generate_plot(folder, 30)
+    
---
+++ covid_19/plotting.py
@@ -0,0 +1,32 @@
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.io import export_png
+from bokeh.models import DatetimeTickFormatter
+from bokeh.palettes import Spectral10
+from covid_19.dataretrieval import get_daily_cases, update_daily_cases_with_forecasts
+
+
+def generate_plot(folder, show_only_last):
+    df_daily = get_daily_cases(folder)
+    df_updated = update_daily_cases_with_forecasts(folder)
+    data_actual = df_daily.dropna()[-show_only_last:]
+    data_forecast = df_updated.dropna()[-show_only_last:]
+    data_rolling = df_updated.rolling(window=7).mean().dropna()[-show_only_last:]
+
+    data_dict = {"Date": data_actual.index, "Value": data_actual.array }
+    data_forecast_dict = {"Date": data_forecast.index, "Value": data_forecast.array}
+    data_rolling_dict = {"Date": data_rolling.index, "Value": data_rolling.array}
+
+    source = ColumnDataSource(data_dict)
+    source_forecast = ColumnDataSource(data_forecast_dict)
+    source_rolling = ColumnDataSource(data_rolling_dict)
+
+    p = figure(x_axis_type="datetime", plot_width=800, plot_height=350)
+    p.line('Date', 'Value', source=source, line_width=1, legend_label="Current data", line_color=Spectral10[1])
+    p.line('Date', 'Value', source=source_forecast, line_dash="dashed", line_width=1, legend_label="Forecast", line_color=Spectral10[9])
+    p.line('Date', 'Value', source=source_rolling, line_width=5, legend_label="7-day rolling average", line_color=Spectral10[0])
+    p.xaxis.formatter = DatetimeTickFormatter(days="%d/%b", months="%d/%b", hours="%d/%b", minutes="%d/%b")
+    p.yaxis.axis_label = "Daily new COVID-19 infections"
+    p.legend.location = "top_left"
+
+    export_png(p, filename=folder + "COVID-19_daily_cases_plot.png")
