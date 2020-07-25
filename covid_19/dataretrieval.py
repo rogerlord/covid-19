a2de4f1c22a8b2be601540fdf8fcbd9ab486ce5e
rogerlord <32603156+rogerlord@users.noreply.github.com>
2020-07-25 14:53:54 +0200

    Add routines

--- covid_19/dataretrieval.py
+++ covid_19/dataretrieval.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datetime
+import numpy as np
+from numpy import linalg
 
 
 def get_latest_rivm_file():
@@ -23,6 +25,33 @@ def get_values_on_date(data_frame, requested_date):
     requested_date_datetime = datetime.datetime.combine(requested_date, datetime.datetime.min.time())
     requested_date_datetime_next_day = requested_date_datetime + datetime.timedelta(days=1)
     return data_frame[requested_date_datetime:requested_date_datetime_next_day]
+
+
+def filter_dataseries(ds, first_date, last_date):
+    return ds[filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1),
+                     ds.index)]
+
+
+def get_scaling_coefficient(lag, df_most_recent, df_lagged_values, first_date, last_date):
+    x = np.array(filter_dataseries(df_lagged_values[str(lag)], first_date, last_date).sort_index().array)
+    y = np.array(filter_dataseries(df_most_recent, first_date, last_date).sort_index().array)
+    x = x[:, np.newaxis]
+
+    scaling, _, _, _ = linalg.lstsq(x, y, rcond=None)
+    return scaling[0]
+
+
+def update_daily_cases_with_forecasts(folder):
+    df_daily_cases = get_daily_cases(folder).sort_index()
+    df_daily_cases_updated = df_daily_cases.copy()
+    df_lagged_values = get_lagged_values(folder).copy().sort_index()
+    first_date = min(df_lagged_values.index).date()
+    last_date = max(df_lagged_values.index).date()
+    last_accurate_date = last_date - datetime.timedelta(days=14)
+    for i in range(len(df_lagged_values.columns)):
+        scaling = get_scaling_coefficient(i, df_daily_cases, df_lagged_values, first_date, last_accurate_date)
+        df_daily_cases_updated.iloc[-(i+1)] = df_daily_cases_updated.iloc[-(i+1)] * scaling
+    return df_daily_cases_updated
 
 
 def update_files(folder):
