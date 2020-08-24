61510f6e1c3299a4345614e83df7c6108d0f6808
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2020-08-24 14:41:43 +0200

    Fix tests

--- covid_19/tests/nl/test_demography.py
+++ covid_19/tests/nl/test_demography.py
@@ -3,5 +3,5 @@ from covid_19.nl.demography import get_population_per_ggd_region
 
 def test_get_population_per_ggd_region():
     population_per_ggd_region = get_population_per_ggd_region()
-    assert population_per_ggd_region["GG0111"] == 586_061
+    assert population_per_ggd_region["GG0111"] == 585_866
     assert len(population_per_ggd_region) == 25
