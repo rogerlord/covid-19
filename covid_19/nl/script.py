5b18d91394352a35bea3437cfb5c02b639c66551
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2020-10-15 22:14:55 +0200

    Updated plots for chainladder model

--- covid_19/nl/script.py
+++ covid_19/nl/script.py
@@ -1,14 +1,16 @@
 import sys
 from covid_19.nl.dataretrieval import get_measures
 from covid_19.nl.plotting import generate_plot_national_cases_per_day, generate_plot_daily_cases_per_ggd_region, \
-    generate_plot_heatmap
+    generate_plot_heatmap, generate_plot_national_cases_per_day_chainladder, generate_plots_chainladder
 from covid_19.nl.updating import update_files, update_measures
+import datetime
 
 
 if __name__ == "__main__":
     folder = sys.argv[1]
     update_files(folder)
     update_measures(get_measures(folder), folder).to_csv(folder + r"data\nl\COVID-19_measures.csv")
-    generate_plot_national_cases_per_day(folder, 30)
+    generate_plot_national_cases_per_day_chainladder(folder, 30)
+    generate_plots_chainladder(folder, datetime.date(2020, 8, 1), 21)
     generate_plot_daily_cases_per_ggd_region(folder, "gross")
     generate_plot_heatmap(folder)
