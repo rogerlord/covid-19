2ed4ad13c73c5ff2343df17f9c502e3b0ea08781
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2020-11-14 17:36:21 +0100

    Pin more versions

--- covid_19/nl/script.py
+++ covid_19/nl/script.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         date_to_run = sys.argv[2]
 
-    update_files(folder, date_to_run)
-    update_measures(get_measures(folder), folder, date_to_run).to_csv(folder + r"data\nl\COVID-19_measures.csv")
-    generate_plot_national_cases_per_day_chainladder(folder, 30)
-    generate_plots_chainladder(folder, datetime.date(2020, 8, 1), 21)
+    #update_files(folder, date_to_run)
+    #update_measures(get_measures(folder), folder, date_to_run).to_csv(folder + r"data\nl\COVID-19_measures.csv")
+    #generate_plot_national_cases_per_day_chainladder(folder, 30)
+    #generate_plots_chainladder(folder, datetime.date(2020, 8, 1), 21)
     generate_plot_daily_cases_per_ggd_region(folder, "gross")
-    generate_plot_heatmap(folder)
+    #generate_plot_heatmap(folder)
