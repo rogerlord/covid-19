ad1fbdcd9960a3661d185537f92338388c4a348b
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2020-12-18 20:18:50 +0100

    Changes to CBS table

--- covid_19/nl/demography.py
+++ covid_19/nl/demography.py
@@ -6,10 +6,10 @@ import os
 def get_population_per_ggd_region():
     # Table 84721NED gives information about regions within the Netherlands
     # See: https://www.cbs.nl/nl-nl/cijfers/detail/84721NED
-    cbs_data = cbsodata.get_data('84721NED', select=['RegioS', 'Code_14', 'Inwonertal_50'])
+    cbs_data = cbsodata.get_data('84721NED', select=['RegioS', 'Code_14', 'Inwonertal_52'])
     cbs_df = pd.DataFrame(cbs_data).groupby("Code_14").sum()
     cbs_df.index = cbs_df.index.str.strip()
-    return cbs_df.to_dict()["Inwonertal_50"]
+    return cbs_df.to_dict()["Inwonertal_52"]
 
 
 def get_ggd_regions():
