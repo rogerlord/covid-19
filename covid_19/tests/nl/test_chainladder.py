45545e79f092f062687cf15e88afbbe62b273169
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2020-12-20 19:11:56 +0100

    Merge branch 'master' into jacobian
    # Conflicts:
    #	covid_19/chainladder.py
    #	covid_19/tests/nl/test_chainladder.py

--- covid_19/tests/nl/test_chainladder.py
+++ covid_19/tests/nl/test_chainladder.py
@@ -11,21 +11,21 @@ def test_reperform_chainladder_nowcasts():
     current_path = os.path.dirname(os.path.realpath(__file__))
     folder = os.path.join(current_path, r"../../../")
 
-    start_date = datetime.date(2020, 10, 9)
+    start_date = datetime.date(2020, 8, 1)
     end_date = datetime.date(2020, 10, 10)
     beta = 0.2
 
     get_lagged_values_func = lambda x: get_lagged_values(folder, x)
 
     corrected_cases_per_day, probabilities = nowcast_cases_per_day(end_date, get_lagged_values_func,
-                                                                   get_cases_per_day_from_data_frame,
-                                                                   GitHubRepository(), beta=beta)
+                                                                   get_cases_per_day_from_data_frame, GitHubRepository(), beta=beta, method="L-BFGS-B")
     avg = pd.Series(corrected_cases_per_day).rolling(window=7).mean().dropna()
     avg.to_csv(r"c:\temp\chainladder.csv")
 
     for i in range(0, (end_date - start_date).days + 1):
         dt = start_date + datetime.timedelta(days=i)
-        corrected_cases_per_day, probabilities = nowcast_cases_per_day(dt, get_lagged_values_func, get_cases_per_day_from_data_frame, GitHubRepository(), beta=beta)
+        corrected_cases_per_day, probabilities = nowcast_cases_per_day(dt, get_lagged_values_func,
+                                                                       get_cases_per_day_from_data_frame, GitHubRepository(), beta=beta, method="L-BFGS-B")
         avg = pd.Series(corrected_cases_per_day).rolling(window=7).mean().dropna()
         print(avg.iloc[-1])
-        # print(avg.iloc[-1] - corrected_cases_per_day[-1]/7 + corrected_cases_per_day[-2]/7)
+        #print(avg.iloc[-1] - corrected_cases_per_day[-1]/7 + corrected_cases_per_day[-2]/7)
