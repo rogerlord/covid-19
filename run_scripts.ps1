d603bae7a8c43d769cdc817093fb70f11bec0c1e
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2021-01-10 16:05:37 +0100

    run_scripts fix

--- run_scripts.ps1
+++ run_scripts.ps1
@@ -1,14 +1,14 @@
 $success = $true
 
-cmd /c python covid_19\\nl\\script.py 
+cmd /c "python covid_19\\nl\\script.py"
 if (-not $?) {$success = $?}
 
-cmd /c python covid_19\\de\\script.py
+cmd /c "python covid_19\\de\\script.py"
 if (-not $?) {$success = $?}
 
-cmd /c python covid_19\\uk\\script.py
+cmd /c "python covid_19\\uk\\script.py"
 if (-not $?) {$success = $?}
 
-.\gitcommit.ps1
+#.\gitcommit.ps1
 
 if (-not $success) {throw "at least one step failed"}
