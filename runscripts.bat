512bc610a4346a1a391dca7bf5333d359c371f3f
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2021-01-11 10:51:49 +0100

    Update error handling in scripts

--- runscripts.bat
+++ runscripts.bat
@@ -1,3 +1,22 @@
+@ECHO ON
+SET ERRORS = 0
 python covid_19\\nl\\script.py .\\
+IF %ERRORLEVEL% NEQ 0 (
+	SET /A ERRORS = ERRORS + 1
+)
+
 python covid_19\\de\\script.py .\\
+IF %ERRORLEVEL% NEQ 0 (
+	SET /A ERRORS = ERRORS + 1
+)
+
 python covid_19\\uk\\script.py .\\
+IF %ERRORLEVEL% NEQ 0 (
+	SET /A ERRORS = ERRORS + 1
+)
+
+powershell gitcommit.ps1
+IF %ERRORLEVEL% NEQ 0 (
+	SET /A ERRORS = ERRORS + 1
+	EXIT /B %ERRORLEVEL%
+)
