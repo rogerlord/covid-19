363c10c2c936c2c2266b8503d1433f40f7ac8930
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2021-02-07 21:10:09 +0100

    More timezone mess

--- covid_19/pandasutils.py
+++ covid_19/pandasutils.py
@@ -9,6 +9,16 @@ def filter_data_frame(data_frame: pd.DataFrame, first_date: datetime.date, last_
 
 
 def filter_series(ds: pd.Series, first_date: datetime.date, last_date: datetime.date) -> pd.DataFrame:
+    if isinstance(first_date, datetime.date) and isinstance(last_date, datetime.date):
+        return ds[list(
+            filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1),
+                   ds.index))]
+
+    if ds.index.tz == first_date.tz and ds.index.tz == last_date.tz:
+        return ds[list(
+            filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1),
+                   ds.index))]
+
     if isinstance(first_date, datetime.datetime):
         if first_date.tzinfo != datetime.timezone.utc:
             first_date_utc = first_date.replace(tzinfo=datetime.timezone.utc)
@@ -17,6 +27,6 @@ def filter_series(ds: pd.Series, first_date: datetime.date, last_date: datetime.
         if last_date.tzinfo != datetime.timezone.utc:
             last_date_utc = last_date.replace(tzinfo=datetime.timezone.utc)
             return filter_series(ds, first_date, last_date_utc)
-    if ds.index.tz.zone != "UTC":
-        ds = ds.tz_localize(datetime.timezone.utc)
-    return ds[list(filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1), ds.index))]
+    if hasattr(ds.index.tz, 'zone'):
+        if ds.index.tz.zone != "UTC":
+            ds = ds.tz_localize(datetime.timezone.utc)
