e1a8e58795fe0f38be5fe2d0406f5ea0d0d40db5
Roger Lord <32603156+rogerlord@users.noreply.github.com>
2021-02-08 11:05:15 +0100

    More timezone issues

--- covid_19/pandasutils.py
+++ covid_19/pandasutils.py
@@ -2,14 +2,15 @@ import datetime
 import pandas as pd
 
 
-def filter_data_frame(data_frame: pd.DataFrame, first_date: datetime.date, last_date: datetime.date = None) -> pd.DataFrame:
+def filter_data_frame(data_frame: pd.DataFrame, first_date: datetime.date,
+                      last_date: datetime.date = None) -> pd.DataFrame:
     if last_date is None:
         return data_frame[first_date.strftime("%Y-%m-%d"):first_date.strftime("%Y-%m-%d")]
     return data_frame[first_date.strftime("%Y-%m-%d"):last_date.strftime("%Y-%m-%d")]
 
 
 def filter_series(ds: pd.Series, first_date: datetime.date, last_date: datetime.date) -> pd.DataFrame:
-    if isinstance(first_date, datetime.date) and isinstance(last_date, datetime.date):
+    if not isinstance(first_date, datetime.datetime) and not isinstance(last_date, datetime.datetime):
         return ds[list(
             filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1),
                    ds.index))]
@@ -30,3 +31,7 @@ def filter_series(ds: pd.Series, first_date: datetime.date, last_date: datetime.
     if hasattr(ds.index.tz, 'zone'):
         if ds.index.tz.zone != "UTC":
             ds = ds.tz_localize(datetime.timezone.utc)
+
+    return ds[list(
+        filter(lambda x: last_date + datetime.timedelta(days=1) > x > first_date - datetime.timedelta(days=1),
+               ds.index))]
